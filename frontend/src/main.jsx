fbdd317fe9c55c161c6815cbe4805aa04f5c2c52
missionreadyhamish <hamishe@missionreadyhq.com>
2025-03-29 17:03:59 +1300

    feat: Chatbox

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import ChatBox from "./components/ChatBox.js";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <ChatBox />
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
